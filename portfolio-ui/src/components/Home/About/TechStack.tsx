6166fa7f4052012d2c0e8e6292a062b348305e47
Noor Mahdi <MahdiN@moe.govt.nz>
2024-05-09 16:44:51 +1200

    logos changes

--- portfolio-ui/src/components/Home/About/TechStack.tsx
+++ portfolio-ui/src/components/Home/About/TechStack.tsx
@@ -1,45 +1,54 @@
 import Logo from "@/components/Home/About/Logo";
+import clsx from "clsx";
 
 export default function TechStack() {
   const techStackLogosFolder = "/tech-logos";
-  type TechStackLogo = { LogoName: string; Src: string };
+  type TechStackLogo = { LogoName: string; Src: string; styles?: string };
   const techStack: TechStackLogo[] = [
     {
       LogoName: "Microsoft Azure",
       Src: `${techStackLogosFolder}/Microsoft_Azure.svg`,
     },
     {
-        LogoName: "C#",
-        Src: `${techStackLogosFolder}/C_Sharp_Logo_2023.svg`,
+      LogoName: "C#",
+      Src: `${techStackLogosFolder}/C_Sharp_Logo_2023.svg`,
     },
     {
-        LogoName: "Backend using .Net stack such as DotNet Core",
-        Src: `${techStackLogosFolder}/NET_Core_Logo.svg`,
+      LogoName: "Backend using .Net stack such as DotNet Core",
+      Src: `${techStackLogosFolder}/NET_Core_Logo.svg`,
     },
     {
       LogoName: "Frontend JavaScript frameworks",
       Src: `${techStackLogosFolder}/Angular_React_Vue_Logo.svg`,
     },
     {
-        LogoName: "Node.js + npm",
-        Src: `${techStackLogosFolder}/Nodejs_logo.svg`,
+      LogoName: "Node.js + npm",
+      Src: `${techStackLogosFolder}/Nodejs_logo.svg`,
     },
     {
-        LogoName: "Sass CSS",
-        Src: `${techStackLogosFolder}/Sass_Logo_Color.svg`,
+      LogoName: "Next.js",
+      Src: `${techStackLogosFolder}/Nextjs_Logo.svg`,
+      styles: "invert opacity-25 hover:opacity-100 w-40",
     },
     {
-        LogoName: "Dockers and containerization",
-        Src: `${techStackLogosFolder}/Docker_logo.svg`,
+      LogoName: "Sass CSS",
+      Src: `${techStackLogosFolder}/Sass_Logo_Color.svg`,
+    },
+    {
+      LogoName: "Dockers and containerization",
+      Src: `${techStackLogosFolder}/docker-mark-blue.svg`,
     },
   ];
   return (
     <ul className="flex flex-row flex-wrap justify-around mt-3 w-full">
       {techStack.map((ts, index) => {
         return (
-          <li key={index} className="pb-5">
+          <li key={index} className="pb-5 mx-2">
             <Logo
-              className="h-[50px] md:h-[65px] w-full grayscale hover:grayscale-0"
+              className={clsx(
+                "h-[50px] md:h-[65px] w-full grayscale hover:grayscale-0",
+                ts.styles
+              )}
               width={150}
               height={70}
               src={ts.Src}

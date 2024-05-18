0e943b94904c8206f041a79899fd97604e3cbd85
Noor Mahdi <MahdiN@moe.govt.nz>
2024-05-19 02:12:51 +1200

    clean up

--- portfolio-ui/src/app/page.tsx
+++ portfolio-ui/src/app/page.tsx
@@ -1,14 +1,5 @@
-import Image from "next/image";
-import { Grand_Hotel } from "next/font/google";
-import { PhoneArrowUpRightIcon } from "@heroicons/react/24/outline";
-import PageHeader from "@/components/PageHeader";
-import Greeting from "@/components/Home/AboveTheFold/Greeting";
-import HeroShowcase from "@/components/Home/AboveTheFold/HeroShowcase";
-import CallToAction from "@/components/Home/AboveTheFold/CallToAction";
 import AboveTheFold from "@/components/Home/AboveTheFold/AboveTheFold";
 import About from "@/components/Home/About";
-
-const grandHotel = Grand_Hotel({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (

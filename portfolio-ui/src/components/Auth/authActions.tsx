ffcb52ab43e7d4f48dff0f31f7613ed3e7c3a66d
Noor Mahdi <MahdiN@moe.govt.nz>
2024-05-20 20:54:23 +1200

    enforce prompt on sign-in

--- portfolio-ui/src/components/Auth/authActions.tsx
+++ portfolio-ui/src/components/Auth/authActions.tsx
@@ -1,8 +1,8 @@
 "use server";
-import { signIn, signOut } from "@/auth"
-export async function signInAction(formData: FormData){
-    await signIn("azure-ad-b2c");
+import { signIn, signOut } from "@/auth";
+export async function signInAction(formData: FormData) {
+  await signIn("azure-ad-b2c", undefined, { prompt: "login" });
 }
-export async function signOutAction(formData: FormData){
-    await signOut();
+export async function signOutAction(formData: FormData) {
+  await signOut();
 }
